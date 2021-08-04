1738ff02fa3a6e4450eced0736f5e1addf9b011e
Mike Wheatley <mikewheatley@hotmail.co.uk>
2021-08-04 22:26:57 +0100

    Add project files.

---
+++ Todo/Pages/Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Todo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty] 
+        public IList<TodoItem> Items { get; set; }
+        [BindProperty]
+        public bool AddItem { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Items = TodoItems.Items;
+            AddItem = false;
+        }
+
+        public void OnPost() 
+        {
+            if (AddItem)
+            {
+                Items.Add(new TodoItem()
+                {
+                    Id=Guid.NewGuid(), 
+                    Text="New Task",
+                    Completed=false
+                });
+            }
+            if (Items.Where(i => i.Delete).Any())
+            {
+                Items.Remove(Items.Where(i => i.Delete).Single());
+            }
+            TodoItems.Items = Items;
+            OnGet();
+            Response.Redirect(Request.Path);
+        }
+    }
+}
---
+++ Todo/TodoItems.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo
+{
+    public static class TodoItems 
+    {
+        public static IList<TodoItem> Items { get; set; } = new List<TodoItem>();
+    }
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public bool Completed { get; set; }
+        public bool Delete { get; set; }
+    }
+}
